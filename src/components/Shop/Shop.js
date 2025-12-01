3815470174a8cce08f9d55a8066e16b867bfe7ec
seam820 <contactwithseam@gmail.com>
2025-12-01 12:08:09 +0600

    succesfully

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
-import fakeData from '../../fakeData/products.JSON';
+import fakeData from '../../fakeData/products.json';
+import './Shop.css'
+import Product from '../Product/Product';
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
     const [products, setProduct]= useState(first10)
     return (
-        <div>
-            <h3>this is</h3>
-            <h2> {products.length}</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                
+                {
+                products.map(pd => <Product product={pd}></Product>)
+                }
+                
+            </div>
+            <div className="cart-container">
+                <h2>This is cart</h2>
+            </div>
             
         </div>
     );
